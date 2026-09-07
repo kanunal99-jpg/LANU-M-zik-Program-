982de53c1ee5a1606aee5286a90448ea1b5923d4
kanunal99-jpg <kanunal99@gmail.com>
2026-09-07 23:17:47 +0300

    release: bump LANU Music preview to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android { namespace = "com.lanu.music"; compileSdk = 35
-    defaultConfig { applicationId = "com.lanu.music"; minSdk = 24; targetSdk = 35; versionCode = 1; versionName = "0.1.0" }
+    defaultConfig { applicationId = "com.lanu.music"; minSdk = 24; targetSdk = 35; versionCode = 2; versionName = "0.2.0" }
     compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
     kotlinOptions { jvmTarget = "17" }
 }
